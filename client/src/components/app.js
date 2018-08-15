bde61cf1c9384591b3e4064e14f363092f40320c
leahchoi <soora913@gmail.com>
2018-08-14 18:11:01 -0700

    completed landing skeleton

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Header from './header';
+import Landing from './landing';
+
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <Header/>
+        <Landing/>
     </div>
 );
 
