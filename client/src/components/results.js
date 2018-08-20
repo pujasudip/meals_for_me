3c47eaa5be7b41dc61d44e9f4415b30fccbd74e8
leahchoi <soora913@gmail.com>
2018-08-20 15:36:01 -0700

    routed pages

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../assets/css/results.css'
-
+import { Link } from 'react-router-dom';
 class Results extends Component {
     constructor(props) {
         super(props);
@@ -9,10 +9,12 @@ class Results extends Component {
     render() {
         return (
             <div className='body'>
+                <Link to='/recipe'>
                 <div className='indivDish'>
                     <h3>Dish Name</h3>
                     <div className='dishPicture'></div>
                 </div>
+                </Link>
                 <button className="search">Display More</button>
             </div>
         )
