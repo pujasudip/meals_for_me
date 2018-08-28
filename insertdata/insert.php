ed06708d75e6db82c42059c053e72f257c72e9aa
sohnOfGod <jsohn000@gmail.com>
2018-08-27 20:23:11 -0700

    finished insert.php!

--- insertdata/insert.php
+++ insertdata/insert.php
@@ -31,7 +31,7 @@ if ($output === false) {
 
 $dishScore = $decoded['spoonacularScore'];
 $dishID = $decoded['id'];
-$dishName = $decoded['title'];
+$dishName = addslashes($decoded['title']);
 $dishTimeToCook = $decoded['readyInMinutes'];
 $dishServings = $decoded['servings'];
 $dishImage = $decoded['image'];
@@ -40,59 +40,94 @@ $dishInstructions = $decoded['analyzedInstructions'];
 $extendedIngredients = $decoded["extendedIngredients"];
 $dishLikes = $decoded["aggregateLikes"];
 
-$eachIngredientID = $decoded["extendedIngredients"][$i]['id'];
-$eachIngredientName = $decoded["extendedIngredients"][$i]['name'];
+//print_r( $instructions);
 
 $vegetarian = $decoded['vegetarian'];
+if($vegetarian){
+    $vegetarian = 1;
+} else{
+    $vegetarian = 0;
+}
 $vegan = $decoded['vegan'];
+if($vegan){
+    $vegan = 1;
+} else{
+    $vegan = 0;
+}
 $glutenFree = $decoded['glutenFree'];
+if($glutenFree){
+    $glutenFree = 1;
+} else{
+    $glutenFree = 0;
+}
 $dairyFree = $decoded['dairyFree'];
+if($dairyFree){
+    $dairyFree = 1;
+} else{
+    $dairyFree = 0;
+}
 $healthScore = $decoded['healthScore'];
 $diets = $decoded['diets'];
 $winePairing = $decoded['winePairing'];
 
 //these are for table input
-$jsonInstructions = json_encode($dishInstructions);
-$jsonIngredients = json_encode($extendedIngredients);
-$jsonWinePairing = json_encode($winePairing);
+$jsonInstructions = addslashes(json_encode($dishInstructions));
+$jsonIngredients = addslashes(json_encode($extendedIngredients));
+$jsonWinePairing = addslashes(json_encode($winePairing));
 $jsonDiets = json_encode($diets);
 
 
-$dataInput = mysqli_query($conn, $query);
 if(!$conn){
     die("Connection Failed: " . mysqli_connect_error());
-} else{
-    echo "connection made";
 }
-
-$recipeIngredientQuery = "
-INSERT INTO `recipe_ingredients`(`recipe_ID`, `ingredient_ID`, `Name`, `Image_url`) 
-VALUES (
-$dishID,$eachIngredientID,$dishName,$dishImage
-)";
+/*print($dishID);
+print($dishScore);
+print($dishName);
+print($dishTimeToCook);
+print($dishServings);
+print($dishImage);
+print($jsonInstructions);
+print($jsonIngredients);
+print($vegetarian);
+print($vegan);*/
 
 $recipeQuery = "
 REPLACE INTO 
 `recipes`(
 `ID`, `Score`, `Name`, `Time`, `Servings`, `Image`, 
 `Instructions`, `Ingredients`, `vegetarian`, `vegan`, 
-`glutenfree`, `dairyfree`, `likes`, `healthscore`, `diets`, `winepairings`) 
-VALUES (
-$dishName,$dishScore,$dishName,$dishTimeToCook,$dishServings,
-$dishImage,$jsonInstructions,$jsonIngredients,$vegetarian,$vegan,
-$glutenFree,$dairyFree,$dishLikes,$healthScore,$jsonDiets,$jsonWinePairing
+`glutenfree`, `dairyfree`, `likes`, `healthscore`, `diets`, `winepairings`) VALUES (
+$dishID,$dishScore,'$dishName',$dishTimeToCook,$dishServings,
+'$dishImage','$jsonInstructions','$jsonIngredients',$vegetarian,$vegan,
+$glutenFree,$dairyFree,$dishLikes,$healthScore,'$jsonDiets','$jsonWinePairing'
 )";
 
-$ingredientsQuery = "
-REPLACE INTO `ingredients`(`ingredient_ID`, `ingredient_name`) VALUES ($eachIngredientID,$eachIngredientName)";
+$dummy = "
+REPLACE INTO 
+`recipes`(
+`ID`, `Score`, `Name`, `Time`, `Servings`, `Image`, 
+`Instructions`, `Ingredients`, `vegetarian`, `vegan`, 
+`glutenfree`, `dairyfree`, `likes`, `healthscore`, `diets`, `winepairings`) 
+VALUES (
+    1123,79,'test',13,4,
+    'testURLimage','{asdasddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasadasdad}','{jsoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaanjsonjson}',true,false,
+    false,true,3,55,'{asdadadsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaad}','{winepaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaairingstest}'
+)";
+
 
 if(mysqli_query($conn, $recipeQuery)){
     echo "Data entry was successful";
 } else{
     echo "Error Recipes: " . $recipeQuery . '<br>' . mysqli_error($conn);
+    //echo 'error';
 }
 
 for($i = 0; $i<count($extendedIngredients); $i++){
+    $eachIngredientID = $decoded["extendedIngredients"][$i]['id'];
+    $eachIngredientName = addslashes($decoded["extendedIngredients"][$i]['name']);
+    $ingredientsQuery = "
+REPLACE INTO `ingredients`(`ingredient_ID`, `ingredient_name`) VALUES ($eachIngredientID,'$eachIngredientName')";
+
     if(mysqli_query($conn, $ingredientsQuery)){
         echo "Data entry was successful";
     } else{
@@ -101,13 +136,21 @@ for($i = 0; $i<count($extendedIngredients); $i++){
 }
 
 for($i = 0; $i<count($extendedIngredients); $i++){
+    $eachIngredientID = $decoded["extendedIngredients"][$i]['id'];
+    $eachIngredientName = addslashes($decoded["extendedIngredients"][$i]['name']);
+
+    $recipeIngredientQuery = "
+REPLACE INTO `recipe_ingredients`(`recipe_ID`, `ingredient_ID`, `Name`, `Image_url`) 
+VALUES (
+$dishID,$eachIngredientID,'$dishName','$dishImage'
+)";
+
     if(mysqli_query($conn, $recipeIngredientQuery)){
         echo "Data entry was successful";
     } else{
         echo "Error REC ING: " . $recipeIngredientQuery . '<br>' . mysqli_error($conn);
     }
 }
-
 
 mysqli_close($conn);
 
