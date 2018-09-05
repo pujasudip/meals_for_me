630866b4d5b9bc399a33ec430fa13d2d5e4d5cb4
sohnOfGod <jsohn000@gmail.com>
2018-09-05 12:35:36 -0700

    fixed queries

--- server/getData.php
+++ server/getData.php
@@ -15,6 +15,7 @@ $countPost = count($_GET);
 switch ($countPost){
     case 1:
         $ingredientOne = $_GET['one'];
+
         $queryForOne = '
 SELECT `recipes`.*
 FROM `recipes`
@@ -22,41 +23,41 @@ JOIN `recipe_ingredients` ON `recipes`.`ID` = `recipe_ingredients`.`recipe_ID`
 JOIN `ingredients` ON `recipe_ingredients`.`ingredient_ID` = `ingredients`.`ingredient_ID`
 WHERE `ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientOne.'%" 
 ORDER BY `recipes`.`Score` DESC';
+
         $query = $queryForOne;
         break;
     case 2:
         $ingredientOne = $_GET['one'];
         $ingredientTwo = $_GET['two'];
+
         $queryForTwo = '
 SELECT `recipes`.*
 FROM `recipes`
 JOIN `recipe_ingredients` ON `recipes`.`ID` = `recipe_ingredients`.`recipe_ID`
 JOIN `ingredients` ON `recipe_ingredients`.`ingredient_ID` = `ingredients`.`ingredient_ID`
 WHERE (`ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientOne.'%" 
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientTwo.'%")
-OR (`ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientTwo.'%" 
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientOne.'%")
+OR `ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientTwo.'%")
+GROUP BY `recipes`.`ID` 
+HAVING COUNT(*) > 1
 ORDER BY `recipes`.`Score` DESC';
+
         $query = $queryForTwo;
         break;
     case 3:
         $ingredientOne = $_GET['one'];
         $ingredientTwo = $_GET['two'];
         $ingredientThree = $_GET['three'];
+
         $queryForThree = '
 SELECT `recipes`.*
 FROM `recipes`
 JOIN `recipe_ingredients` ON `recipes`.`ID` = `recipe_ingredients`.`recipe_ID`
 JOIN `ingredients` ON `recipe_ingredients`.`ingredient_ID` = `ingredients`.`ingredient_ID`
 WHERE (`ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientOne.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientTwo.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientThree.'%")
-OR (`ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientTwo.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientOne.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientThree.'%")
-OR (`ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientThree.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientOne.'%"
-AND `ingredients`.`ingredient_name` NOT LIKE '.'"%'.$ingredientTwo.'%")
+OR `ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientTwo.'%"
+OR `ingredients`.`ingredient_name` LIKE '.'"%'.$ingredientThree.'%")
+GROUP BY `recipes`.`ID` 
+HAVING COUNT(*) > 2
 ORDER BY `recipes`.`Score` DESC
 ';
         $query = $queryForThree;
@@ -89,9 +90,6 @@ if (empty($result)) {
 $jsonOutput = json_encode($output);
 
 print_r($jsonOutput)
-
-
-
 
 
 ?>
