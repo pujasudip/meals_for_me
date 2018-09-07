6cf459e7889f9ed1b97cee511f42a30823175b21
sohnOfGod <jsohn000@gmail.com>
2018-09-07 11:38:33 -0700

    made script files for cleaning database

--- insertdata/deleteNull.php
+++ insertdata/deleteNull.php
@@ -1,33 +1,18 @@
 <?php
+
 require_once('mysql_connect.php');
 
 $recipeDelete = "
 DELETE FROM `recipes` 
 WHERE (`Instructions` = '[]' OR `Image` = '' OR `Ingredients` = '[]')";
 
-$recIngDelete = "
-DELETE `recipe_ingredients` 
-FROM `recipe_ingredients` 
-LEFT JOIN `recipes` 
-ON `recipes`.ID = `recipe_ingredients`.`recipe_ID` 
-WHERE `recipes`.ID IS null";
-
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$result = mysqli_query($conn, $recipeDelete);
 
-if (mysqli_query($conn, $result)) {
+if (mysqli_query($conn, $recipeDelete)) {
     echo "Recipe deleted successfully";
-} else {
-    echo "Error deleting record: " . mysqli_error($conn);
-}
-
-$result2 = mysqli_query($conn, $recIngDelete);
-
-if (mysqli_query($conn, $result2)) {
-    echo "REC ING deleted successfully";
 } else {
     echo "Error deleting record: " . mysqli_error($conn);
 }
