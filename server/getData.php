95bbb8047342b03f843539738550745b1b8ef2bb
sohnOfGod <jsohn000@gmail.com>
2018-09-08 17:37:01 -0700

    added favorites php stuff

--- server/getData.php
+++ server/getData.php
@@ -8,7 +8,7 @@ require_once('mysql_server_connect.php');
 if($conn->connect_error){
     die("failed to connect to MYSQL: (" . $conn->connect_errorno .") " . $conn->connect_error);
 }
-
+// FUNCTION TO REPLACE GET_RESULTS()
 function fetchAssocStatement($stmt){
     if ($stmt->num_rows > 0) {
         $result = array();
@@ -21,28 +21,28 @@ function fetchAssocStatement($stmt){
         if ($stmt->fetch()) {
             return $result;
         }
-
         return null;
     }
 }
-
+//SWITCH STATEMENT TO DIRECT WHICH QUERY TO USE AND GET SEARCH RESULTS
 $countPost = count($_GET);
 switch ($countPost){
     case 1:
         $ingredientOne = '%'.addslashes($_GET['one']).'%';
+        $numOfResults = 10*intval($_GET['number']);
         $queryForOne = '
 SELECT `recipes`.*
 FROM `recipes`
 JOIN `recipe_ingredients` ON `recipes`.`ID` = `recipe_ingredients`.`recipe_ID`
 JOIN `ingredients` ON `recipe_ingredients`.`ingredient_ID` = `ingredients`.`ingredient_ID`
 WHERE `ingredients`.`ingredient_name` LIKE (?)
-ORDER BY `recipes`.`Score` DESC';
+ORDER BY `recipes`.`likes` DESC LIMIT 10 OFFSET (?)';
 
         if($stmt = $conn->prepare($queryForOne)){
-            $stmt->bind_param('s', $ingredientOne);
+            $stmt->bind_param('si', $ingredientOne,$numOfResults);
             $stmt->execute();
             $stmt->store_result();
-            while ($row = fetchAssocStatement($stmt)) {
+            while ($row = fetchAssocStatement($stmt)){
                 $results[] = $row;
             }
             $jsonOutput = json_encode($results);
@@ -54,6 +54,8 @@ ORDER BY `recipes`.`Score` DESC';
     case 2:
         $ingredientOne = '%'.addslashes($_GET['one']).'%';
         $ingredientTwo = '%'.addslashes($_GET['two']).'%';
+        $numOfResults = 10*intval($_GET['number']);
+
         $queryForTwo = '
 SELECT `recipes`.*
 FROM `recipes`
@@ -63,14 +65,14 @@ WHERE (`ingredients`.`ingredient_name` LIKE (?)
 OR `ingredients`.`ingredient_name` LIKE (?) )
 GROUP BY `recipes`.`ID`
 HAVING COUNT(*) > 1
-ORDER BY `recipes`.`Score` DESC';
+ORDER BY `recipes`.`likes` DESC LIMIT 10 OFFSET (?)';
 
         if($stmt = $conn->prepare($queryForTwo)){
-            $stmt->bind_param('ss', $ingredientOne,$ingredientTwo);
+            $stmt->bind_param('ssi', $ingredientOne,$ingredientTwo,$numOfResults);
             $stmt->execute();
             $stmt->store_result();
 
-            while ($row = fetchAssocStatement($stmt)) {
+            while ($row = fetchAssocStatement($stmt)){
                 $results[] = $row;
             }
             $jsonOutput = json_encode($results);
@@ -83,6 +85,8 @@ ORDER BY `recipes`.`Score` DESC';
         $ingredientOne = '%'.addslashes($_GET['one']).'%';
         $ingredientTwo = '%'.addslashes($_GET['two']).'%';
         $ingredientThree = '%'.addslashes($_GET['three']).'%';
+        $numOfResults = 10*intval($_GET['number']);
+
         $queryForThree = '
 SELECT `recipes`.*
 FROM `recipes`
@@ -93,13 +97,13 @@ OR `ingredients`.`ingredient_name` LIKE (?)
 OR `ingredients`.`ingredient_name` LIKE (?) )
 GROUP BY `recipes`.`ID`
 HAVING COUNT(*) > 2
-ORDER BY `recipes`.`Score` DESC';
+ORDER BY `recipes`.`likes` DESC LIMIT 10 OFFSET (?)';
 
         if($stmt = $conn->prepare($queryForThree)){
-            $stmt->bind_param('sss', $ingredientOne,$ingredientTwo,$ingredientThree);
+            $stmt->bind_param('sssi', $ingredientOne,$ingredientTwo,$ingredientThree,$numOfResults);
             $stmt->execute();
             $stmt->store_result();
-            while ($row = fetchAssocStatement($stmt)) {
+            while ($row = fetchAssocStatement($stmt)){
                 $results[] = $row;
             }
             $jsonOutput = json_encode($results);
@@ -112,9 +116,7 @@ ORDER BY `recipes`.`Score` DESC';
         die('invalid search');
         break;
 }
-
 $conn->close();
-
 ?>
 
 
