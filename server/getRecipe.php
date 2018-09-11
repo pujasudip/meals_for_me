55234be61e28368447e024deb8f1087060105994
sohnOfGod <jsohn000@gmail.com>
2018-09-11 11:28:54 -0700

    recipe query details

---
+++ server/getRecipe.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+require_once('mysql_server_connect.php');
+
+if($conn->connect_errno){
+    die('Bad connection');
+}
+
+if(empty($_GET)){
+    die('No recipe ID');
+}
+
+$id = $_GET['id'];
+
+$query = "SELECT * FROM `recipes` WHERE `recipes`.`ID` = . $id";
+
+if($result = $conn->query($query)){
+    print($result);
+} else{
+    print('invalid recipe ID');
+}
+
+$result->close();
+
+?>
