746f68204c7e5e745e462d2e56c5c38a17882ade
leahchoi <soora913@gmail.com>
2018-09-11 15:19:18 -0700

    extra info on recipe page

--- client/src/components/directions.js
+++ client/src/components/directions.js
@@ -4,18 +4,23 @@ import { connect } from 'react-redux';
 class Directions extends Component{
     render(){
         const directions = JSON.parse(this.props.directions.Instructions)[0].steps;
+        console.log("Unformatted steps: ", directions);
         let directionList = '';
 
         // console.log('directions:', directions);
 
         if(directions){
-            let directionListArray = directions.map((ele, index)=>{
-                return ele.step;
-            });
-            directionListArray = directionListArray.join('');
-            directionListArray = directionListArray.split('.');
-            directionList = directionListArray.map((ele, index)=>{
-                return <li key={index}>{ele}</li>;
+            // let directionListArray = directions.map((ele, index)=>{
+            //     return ele.step;
+            // });
+            // directionListArray = directionListArray.join('');
+            // directionListArray = directionListArray.split('.');
+            // directionList = directionListArray.map((ele, index)=>{
+            //     return <li key={index}>{ele}</li>;
+            // });
+
+            directionList = directions.map((ele, index)=>{
+                return <li key={index}>{ele.step}</li>;
             });
 
         }
