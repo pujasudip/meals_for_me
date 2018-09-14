34b38019ebd1952d77d6f7385ff715ca8d3591ee
leahchoi <soora913@gmail.com>
2018-09-13 17:52:38 -0700

    Bundled client and ready to deploy on production server

---
+++ client/dist/api/server/testData.php
@@ -0,0 +1,16 @@
+<?php
+
+print_r($_POST);
+print('<br>');
+print(count($_POST));
+
+
+$ingredientOne = $_POST['one'];
+$ingredientTwo = $_POST['two'];
+print($ingredientOne);
+print($ingredientTwo);
+
+
+
+?>
+
